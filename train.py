0b057894091ba6bdef12b178ec3b85b61a84d2e3
jason <jasonwang924@hotmail.com>
2018-06-26 20:02:28 +0800

    modify code for GPU

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import numpy as np
 from HTK import HTKFile
 import keras.backend as K
 from keras.models import Model
-from keras.layers import Input, Dense, Conv1D, Reshape, GRU, Lambda, TimeDistributed
+from keras.layers import Input, Dense, Conv1D, Reshape, GRU, Lambda, TimeDistributed, CuDNNGRU
 from keras.preprocessing.sequence import pad_sequences
 from keras.callbacks import TerminateOnNaN, LambdaCallback
 
@@ -67,7 +67,8 @@ label_length = Input(shape=(1,))
 x = TimeDistributed(Dense(128, activation='relu'))(inputs)
 x = TimeDistributed(Dense(128, activation='relu'))(x)
 x = TimeDistributed(Dense(128, activation='relu'))(x)
-x = GRU(128, return_sequences=True, activation='relu')(x)
+# x = GRU(128, return_sequences=True, activation='relu')(x)
+x = CuDNNGRU(128, return_sequences=True)(x)
 x = TimeDistributed(Dense(128, activation='relu'))(x)
 y = TimeDistributed(Dense(11, activation='softmax'))(x)
 loss_output = Lambda(ctc_wrapper)([train_labels, y, input_length, label_length])
@@ -83,7 +84,7 @@ test_model = Model(inputs=inputs, outputs=y)
 # debug_cb = LambdaCallback(on_batch_end=debug_pred)
 
 model.fit([train_x, train_y, input_lengths, label_lengths], np.zeros(train_x.shape[0]),
-          batch_size=batch_size, epochs=epochs, validation_split=0.05)
+          batch_size=batch_size, epochs=epochs)
 
 # only save the model for testing
 test_model.save("model.h5")
